601d465717ad6c81d8d0c950433df497243ce050
TANI Kojiro <kojiro0531@gmail.com>
2018-01-24 22:20:28 +0900

    Reimplement debugger

---
+++ src/debugger.lisp
@@ -0,0 +1,50 @@
+(in-package :cl-repl)
+
+(defun debugger-loop (choices condition)
+  (format t (color *section-color* "~%restarts:~%"))
+  (let ((n (length choices)) (i))
+    (do ((c choices (cdr c)) (i 0 (+ i 1)))
+        ((null c))
+      (format t (color *restart-candidates-color* "~2d. [~a] ~a~%") i (restart-name (car c)) (car c)))
+    (print-backtrace condition n)
+    (terpri)
+    (loop
+      (let ((input (read-input)))
+        (if (typep input `(integer 0 ,n))
+            (progn
+              (setf i input)
+              (if (= i n)
+                  (progn (show-backtrace condition))
+                  (return)))
+            (eval-print input))))
+    (find-restart (nth i choices))))
+
+(defun show-backtrace (condition)
+  (trivial-backtrace:print-backtrace condition))
+
+(defun get-backtrace (condition)
+  (split-sequence:split-sequence #\NEWLINE
+                                 (with-output-to-string (out)
+                                   (uiop/image:print-backtrace
+                                    :stream out
+                                    :condition condition))))
+
+(defun print-backtrace (condition n)
+  (finish-output)
+  (let ((backtrace (get-backtrace condition)))
+    (loop for b in backtrace
+          for i from 0
+          while (< i 5)
+          do (format t (color *backtrace-color* " ~a~%") b)
+          initially (format t (color *section-color* "~%backtrace:~%"))
+          finally (format t " --more-- (type ~d)~%" n))))
+
+(defun debugger (condition me-or-my-encapsulation)
+  (incf *debugger-level*)
+  (format t (color *condition-color* "~a~%  [Condition of type ~a]~%") condition (type-of condition))
+  (let ((restart (debugger-loop (compute-restarts) condition)))
+    (if (not restart) (error "The debugger got an error."))
+    (let ((*debugger-hook* me-or-my-encapsulation))
+      (decf *debugger-level*)
+      (invoke-restart-interactively restart))))
+
