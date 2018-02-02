5c889de0734c1184d0a26bcca84f0250171565c5
TANI Kojiro <kojiro0531@gmail.com>
2018-02-02 10:06:57 +0900

    Add some exports for configuration

--- src/package.lisp
+++ src/package.lisp
@@ -1,5 +1,13 @@
 (in-package :cl-user)
 (defpackage :cl-repl
   (:use :cl)
-  (:export main))
+  (:export main
+           define-magic
+           message-from-magic
+           define-color-scheme
+           color-scheme
+           *default-prompt-function*
+           *debugger-prompt-function*))
 
+(defpackage :repl-user
+  (:use :cl :cl-repl))
