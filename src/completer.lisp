71ee5c9562fa68ffc759f9a8a383b502e6cc5a7c
TANI Kojiro <kojiro0531@gmail.com>
2018-02-03 22:17:21 +0900

    Don't add white space when completion result is a package

--- src/completer.lisp
+++ src/completer.lisp
@@ -1,65 +1,61 @@
 (in-package :cl-repl)
 
-(defun common-prefix (items)
-  (subseq
-   (car items)
-   0
-   (apply
-    #'min
-    (mapcar
-     #'(lambda (i) (or (mismatch (car items) i) (length i)))
-     (cdr items)))))
+(defun get-package-for-search (text)
+  (let ((pos))
+    (cond
+      ((setf pos (search "::" text))
+       (list  (subseq text  (+ pos 2)) (subseq text 0 pos) nil))
+      ((setf pos (position #\: text))
+       (if (zerop pos)
+         (list text nil t)
+         (list (subseq text (1+ pos)) (subseq text 0 pos) t)))
+      (t (list text nil  t)))))
 
-(defun package-prefix (str)
-  (cond
-    ((let ((pos (search "::" str)))
-       (when pos
-         (list (subseq str (+ pos 2)) (subseq str 0 pos) nil))))
-    ((let ((pos (position #\: str)))
-       (when pos
-         (list
-          (subseq str (+ pos 1))
-          (if (zerop pos)
-              "KEYWORD"
-              (subseq str 0 pos))
-          t))))
-    (t (list str nil nil))))
+(defun list-external-symbols (sym-name pkg-name)
+  (loop :for sym :being :the :external-symbols :of (find-package pkg-name)
+        :collect (format nil "~(~a:~a~)" pkg-name sym)))
+
+(defun list-internal-symbols (sym-name pkg-name)
+  (loop :for sym :being :the :symbols :of (find-package pkg-name)
+        :collect (format nil "~(~a::~a~)" pkg-name sym)))
+
+(defun list-symbols-and-packages (sym-name)
+  (concatenate 'list
+    (loop :for pkg :in (list-all-packages)
+          :append (loop :for name :in (package-nicknames pkg)
+                        :collect (format nil "~(~a:~)" name))
+          :collect (format nil "~(~a:~)" (package-name pkg)))
+    (loop :for sym :being :the :symbols :of *package*
+          :collect (string-downcase sym))
+    (loop :for kw :being :the :symbols :of (find-package "KEYWORD")
+          :collect (format nil ":~(~a~)" kw))))
+
+(defun select-completions (text items)
+  (setf items
+    (loop :for item :in items
+          :when (alexandria:starts-with-subseq text item)
+                :collect item))
+  (unless (cdr items)
+    (setf rl:*completion-append-character*
+      (if (alexandria:ends-with-subseq ":" (car items)) #\nul #\space))
+    (return-from select-completions items))
+  (cons
+    (subseq (car items) 0
+      (loop :for item :in (cdr items)
+            :minimize (or (mismatch (car items) item) (length item))))
+    items))
 
 (defun completer (text start end)
   (declare (ignore start end))
-  (if (string-equal text "")
-      (return-from completer '("")))
-  (let ((text (string-upcase text))
-        (els))
-    (flet ((body (sym text prefix)
-             (let ((name (string sym)))
-               (when (eql 0 (search text name))
-                 (push (format nil "~(~a~a~)" prefix name)
-                       els)))))
-      (destructuring-bind (symbol-name package external-p)
-          (package-prefix text)
-        (when (and package (not (find-package package)))
-          (return-from completer nil))
-        (cond ((and package external-p)
-               (do-external-symbols (sym package)
-                 (body sym symbol-name
-                       (if (equal (package-name :keyword)
-                                  (package-name package))
-                           ":"
-                           (format nil "~a:" package)))))
-              (package
-               (do-symbols (sym package)
-                 (body sym symbol-name (format nil "~a::" package))))
-              (t
-               (do-symbols (sym *package*)
-                 (body sym symbol-name ""))
-               (dolist (package (list-all-packages))
-                 (body (format nil "~a:" (package-name package))
-                       symbol-name "")
-                 (dolist (package-name (package-nicknames package))
-                   (body (format nil "~a:" package-name)
-                         symbol-name "")))))))
-    (if (cdr els)
-        (cons (common-prefix els) els)
-        els)))
-
+  (when (string-equal text "")
+    (return-from completer nil))
+  (destructuring-bind (sym-name pkg-name external-p)
+    (get-package-for-search (string-upcase text))
+    (when (and pkg-name (not (find-package pkg-name)))
+      (return-from completer nil))
+    (select-completions
+      (string-downcase text) 
+      (cond
+        ((zerop (length pkg-name)) (list-symbols-and-packages sym-name))
+        (external-p (list-external-symbols sym-name pkg-name))
+        (t (list-internal-symbols sym-name pkg-name))))))
