780b61f4938e7ebbc6068ee8cd74183b2a72f3f0
TANI Kojiro <kojiro0531@gmail.com>
2018-02-16 11:36:44 +0900

    Create util.lisp

---
+++ src/util.lisp
@@ -0,0 +1,37 @@
+(in-package :cl-repl)
+
+(defun split-lines (text)
+  (ppcre:split "\\n+" text))
+
+(defun split-space (text)
+  (ppcre:split "\\s+" text))
+
+(defun string-starts-with (str prefix)
+  (when (>= (length str) (length prefix))
+    (string= str prefix :start1 0 :end1 (length prefix))))
+  
+(defun string-ends-with (str suffix)
+ (when (>= (length str) (length suffix))
+   (string= str suffix :start1 (- (length str) (length suffix)))))
+
+(defmacro string-case (str &rest forms)
+  `(case (intern ,str)
+     ,@(loop :for (s f) :in forms
+             :if (stringp s) :collect `(,(intern s) ,f)
+             :else :collect `(,s ,f))))
+
+(defmacro with-cursor-hidden (&body body)
+  `(unwind-protect
+     (progn
+       (format t "~c[?25l" #\esc)
+       ,@body)
+     (format t "~c[?25h" #\esc)))
+
+(defun flush-screen ()
+  (with-cursor-hidden
+    (format t "~c[H~@*~c[j" #\esc)
+    (finish-output)))
+
+
+
+
